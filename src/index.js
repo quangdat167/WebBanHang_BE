50a9aabf283845d625640033e064d40a2a5c63d1
DatPhit <datvg167@gmail.com>
2023-05-04 10:41:35 +0700

    basic init for nodejs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
 });
+
+//HTTP logger
+app.use(morgan('dev'));
+
+// Template engine`
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+// Static file path
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
